d6188cbd720bad9911f59b117f08f93205395da7
William Tremblay <tremwil@gmail.com>
2018-12-15 22:08:47 -0500

    add LibHarmony

--- CPTools/CPTools.cs
+++ CPTools/CPTools.cs
@@ -15,7 +15,6 @@ namespace CPTools
         ModLoader.ModLoader loader;
 
         bool checkpointsOn = true;
-        bool checkpointsAuto = false;
 
         int lvlLast = 0;
         Vector3 prvCheckpt;
@@ -34,31 +33,12 @@ namespace CPTools
         {
             if (lvlLast != Game.instance.currentLevelNumber)
             {
-                checkpointsOn = false;
-                checkpointsAuto = false;
+                checkpointsOn = true;
             }
             lvlLast = Game.instance.currentLevelNumber;
-
-            if (checkpointsAuto)
-            {
-                if (Human.instance == null) { return; }
-
-                if (Human.instance.onGround && !onGround)
-                {
-                    onGround = true;
-                    lastOnGround = Time.time;
-                }
-                onGround = Human.instance.onGround;
-
-                if (onGround && Time.time - lastOnGround > timeAutosave)
-                {
-                    Game.currentLevel.checkpoints[0].position = Human.instance.transform.position + 50 * Vector3.up;
-                    lastOnGround = Time.time;
-                }
-            }
         }
 
-        public HashSet<string> CommandNames => new HashSet<string>() { "cp-toggle", "cp-auto" };
+        public HashSet<string> CommandNames => new HashSet<string>() { "cp-toggle", "cp-set" };
 
         public void OnCommandTyped(string cmd, string[] args)
         {
@@ -72,7 +52,6 @@ namespace CPTools
                     loader.LogLine("Checkpoints turned " + (checkpointsOn ? "On" : "Off"));
                     if (checkpointsOn)
                     {
-                        checkpointsAuto = false;
                         Game.currentLevel.checkpoints[0].position = prvCheckpt;
                     }
 
@@ -83,27 +62,10 @@ namespace CPTools
                     }
                     break;
 
-                case "cp-auto":
+                case "cp-set":
                     Game.instance.currentCheckpointNumber = 0;
                     Game.instance.currentSolvedCheckpoints.Clear();
 
-                    checkpointsAuto = !checkpointsAuto;
-                    if (checkpointsAuto)
-                    {
-                        checkpointsOn = false;
-                        prvCheckpt = Game.currentLevel.checkpoints[0].position;
-                    }
-                    else
-                    {
-                        checkpointsOn = true;
-                        Game.currentLevel.checkpoints[0].position = prvCheckpt;
-                    }
-
-                    cps = Game.FindObjectsOfType<Checkpoint>();
-                    foreach (Checkpoint cp in cps)
-                    {
-                        cp.enabled = !checkpointsAuto;
-                    }
                     break;
             }
         }
